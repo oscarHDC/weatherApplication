b1bcb929c6ca373b26213d23ca195a2f6607590a
oscarHDC <oscarhdec01@gmail.com>
2023-04-02 22:41:38 +0200

    Creating icon component, changing font

---
+++ src/components/AppIcon.vue
@@ -0,0 +1,40 @@
+<template>
+<img v-bind="bind" :style="style" src="" >
+
+</template>
+
+<script lang="ts">
+import { StyleValue, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AppIcon',
+  props: {
+    src: {
+      type: String,
+      default: undefined
+    },
+    size: {
+      type: String,
+      default: '16px'
+    }
+  },
+
+  computed: {
+    bind() {
+      return {
+        src: this.src,
+        alt: this.src,
+      }
+    },
+
+    style(): StyleValue{
+      return{
+        width: this.size,
+        height: this.size,
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
